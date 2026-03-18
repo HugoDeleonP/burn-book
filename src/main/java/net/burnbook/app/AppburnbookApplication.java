63fc5d05d9982317c9a59d97dcc7408f7f64c8ff
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-03-18 14:38:54 -0300

    MIC-1 chore: adiciona estrutura inicial

---
+++ src/main/java/net/burnbook/app/AppburnbookApplication.java
@@ -0,0 +1,13 @@
+package net.burnbook.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppburnbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppburnbookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/burnbook/app/AppburnbookApplicationTests.java
@@ -0,0 +1,13 @@
+package net.burnbook.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppburnbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
