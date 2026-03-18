5fa35fc7382ef1400c7d5591c73200ad55a98fbf
Hugo Deleon <hugo_paim@estudante.sesisenai.org.br>
2026-03-18 19:50:54 -0300

    MIC-01 feat: desenvolve os models (#2)
    * MIC-01 feat: implementa os models, conforme o diagrama de classe
    
    * MIC-01 feat: adiciona foto de perfil como atributo do usuário

---
+++ src/main/java/net/burnbook/app/model/Curtida.java
@@ -0,0 +1,33 @@
+package net.burnbook.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comentario")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Curtida {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "publicacao_id")
+    private Publicacao publicacao;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    public Curtida(Publicacao publicacao, Usuario usuario) {
+        this.publicacao = publicacao;
+        this.usuario = usuario;
+    }
+}
