2ca320b98d9df968fa37a60d00acb9c350efdc19
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-03-26 15:53:10 -0300

    MIC-30 feat: implementa Exceptions personalizadas nas classes Service e adiciona lógicas faltantes: idade do usuário, existencia de CPF Email e CPF

--- src/main/java/net/burnbook/app/service/UsuarioService.java
+++ src/main/java/net/burnbook/app/service/UsuarioService.java
@@ -2,10 +2,16 @@ package net.burnbook.app.service;
 
 import net.burnbook.app.dto.usuario.UsuarioDTORequest;
 import net.burnbook.app.dto.usuario.UsuarioDTOResponse;
+import net.burnbook.app.infra.exception.model.ConflitoException;
+import net.burnbook.app.infra.exception.model.EntidadeNaoEncontradaException;
+import net.burnbook.app.infra.exception.model.RegraDeNegocioException;
 import net.burnbook.app.mapper.UsuarioMapper;
 import net.burnbook.app.model.Usuario;
 import net.burnbook.app.repository.UsuarioRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
 
 @Service
 public class UsuarioService {
@@ -19,6 +25,23 @@ public class UsuarioService {
     }
 
     public UsuarioDTOResponse cadastrar(UsuarioDTORequest usuarioDto){
+
+        if(Period.between( usuarioDto.dataNascimento(), LocalDate.now()).getYears() < 18 ){
+            throw new RegraDeNegocioException("O Usuário deve ser maior de idade.");
+        }
+
+        if(usuarioRepository.existsByEmail(usuarioDto.email())){
+            throw new ConflitoException("Email já cadastrado!");
+        }
+
+        if(usuarioRepository.existsByUsername(usuarioDto.username())){
+            throw new ConflitoException("Username já cadastrado!");
+        }
+
+        if(usuarioRepository.existsByCpf(usuarioDto.cpf())){
+            throw new ConflitoException("CPF já cadastrado!");
+        }
+
         Usuario usuarioDatabase = usuarioMapper.paraEntidade(usuarioDto);
 
         return usuarioMapper.paraDto(usuarioRepository.save(usuarioDatabase));
@@ -28,7 +51,7 @@ public class UsuarioService {
     public UsuarioDTOResponse buscarPefilPorId(Long usuarioId){
 
         Usuario usuarioBuscado = usuarioRepository.findById(usuarioId)
-                .orElseThrow( () -> new RuntimeException("Usuário não encontrado"));
+                .orElseThrow( () -> new EntidadeNaoEncontradaException("Usuário não encontrado"));
 
         return usuarioMapper.paraDto(usuarioBuscado);
     }
@@ -36,7 +59,7 @@ public class UsuarioService {
     public UsuarioDTOResponse adicionarFotoPerfil(Long usuarioId, String fotoUrl){
 
         Usuario usuarioBuscado = usuarioRepository.findById(usuarioId)
-                .orElseThrow( () -> new RuntimeException("Usuário não encontrado"));
+                .orElseThrow( () -> new EntidadeNaoEncontradaException("Usuário não encontrado"));
 
         usuarioBuscado.setFotoPerfilUrl(fotoUrl);
 
