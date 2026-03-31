c820c93fae4b85de2b4b5afab1aee67d04d07c04
HUGO DELEON GIMINIANI DE SOUZA PAIM <hugo_paim@estudante.sesisenai.org.br>
2026-03-31 20:52:24 -0300

    MIC-31 feat: adiciona campo de ID no JWT

--- src/main/java/net/burnbook/app/infra/security/auth/JwtService.java
+++ src/main/java/net/burnbook/app/infra/security/auth/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import net.burnbook.app.infra.security.auth.model.UsuarioPrincipal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -39,7 +40,10 @@ public class JwtService {
     }
 
     public String gerarToken(UserDetails userDetails){
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) userDetails;
+
         return Jwts.builder()
+                .claim("id", usuarioPrincipal.getId())
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + expiration))
